c48bf9f7ce90ae07f3e945754ea6630d2005583d
gump2012 <yige2002@tom.com>
2014-11-28 16:53:31 +0800

    modify

--- indexpush.js
+++ indexpush.js
@@ -5,7 +5,23 @@ var server = require("./server");
 var router = require("./router");
 var index = require("./indexHandle/indexHandle");
 
+var apn = require('apn');
+
 var handle = {};
 handle["/"] = index.start;
 
 server.start(router.route,handle);
+
+var options = {cert:'/root/apppush/certificate/cert.pem',key:'/root/apppush/certificate/key.pem'};
+
+var apnConnection = new apn.Connection(options);
+var myDevice = new apn.Device('9f07dad7aee6e3f13fbaa645c4d9e67e4fbcc2b1d08d25cd15910716460946f2');
+var note = new apn.Notification();
+
+//note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+note.badge = 1;
+note.sound = "";
+note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
+note.payload = {'messageFrom': 'Caroline'};
+
+apnConnection.pushNotification(note, myDevice);
