195233f46eee4f350b05f00c56e94368dc375d65
gutao <2437987361@qq.com>
2014-12-24 12:27:31 +0800

    添加资源处理

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 /**
  * Created by gump on 11/17/14.
  */
+var res = require("./res/res");
+
 exports.route = function (handle,pathname,response,request){
     request.setEncoding('utf8');
     console.log(request.url);
@@ -8,9 +10,6 @@ exports.route = function (handle,pathname,response,request){
     if(typeof handle[pathname] === 'function'){
         handle[pathname](response,request);
     }else{
-        console.log("No request handler found for " + pathname);
-        response.writeHead(404,{"Content-Type":"text/plain"});
-        response.write("404 Not found");
-        response.end();
+        res.visitRes(request,response);
     }
 }
