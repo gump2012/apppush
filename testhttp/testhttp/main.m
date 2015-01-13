4cfb6daf52fdfbcbf5b5d283b2d29252b53a0ea7
gutao <2437987361@qq.com>
2015-01-13 17:39:45 +0800

    modify

--- testhttp/testhttp/main.m
+++ testhttp/testhttp/main.m
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSLog(@"Hello, World!");
         
-        NSURL *url = [NSURL URLWithString:@"http://latest.toupai360.com:2005/send"];
+        NSURL *url = [NSURL URLWithString:@"http://www.kkk885.cn/send"];
         
         //第二步，创建请求
         
@@ -24,7 +24,7 @@ int main(int argc, const char * argv[]) {
         
         [request setHTTPMethod:@"POST"];//设置请求方式为POST，默认为GET
         
-        NSString *str =@"msgid=1&userid=['07020f2aa48']&message=哦也&msgtype=txt&deviceSysType=android\
+        NSString *str =@"msgid=3&userid=['92e52b79fc57c25ef504dc9e983ecb17a4f081a146e60636ca995de41e2aa82a']&message=哦也&msgtype=txt&deviceSysType=ios\
 &sound=dudu.m4a";
      //设置参数
         
