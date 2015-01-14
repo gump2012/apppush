274546cace246361bc15bad97b2610aff8012f95
gutao <2437987361@qq.com>
2015-01-14 15:47:36 +0800

    bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * Created by gump on 11/17/14.
  */
 var http = require("http");
-
+var url = require("url");
 exports.start = function (route,handle){
     function onRequest(request,response){
         var pathname = url.parse(request.url).pathname;
