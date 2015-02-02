25c14bf9e1b8d6acd08d149f3be8b54024771e92
gutao <2437987361@qq.com>
2015-02-02 20:13:23 +0800

    modify

--- testhttp/testhttp/main.m
+++ testhttp/testhttp/main.m
@@ -26,8 +26,8 @@ int main(int argc, const char * argv[]) {
         
         [request setHTTPMethod:@"POST"];//设置请求方式为POST，默认为GET
         
-        NSString *str =@"msgid=9&userid=['07020f2aa48']&message=天啊&msgtype=txt&deviceSysType=android\
-&sound=dudu.m4a";
+        NSString *str =@"msgid=9&userid=['92e52b79fc57c25ef504dc9e983ecb17a4f081a146e60636ca995de41e2aa82a']&message=天啊&msgtype=txt&deviceSysType=ios\
+&sound=dudu.m4a&addressor=frank&rank=1&truncate=0";
      //设置参数
         
         NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
